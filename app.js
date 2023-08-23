ff4f1731aaa43a8fd6548e3c104669c0fd327db5
Alina Mats <npnote8@gmail.com>
2023-08-22 21:32:54 -0700

    answers for lesson 11

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.use(express.static("public"));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -69,7 +69,8 @@ const deleteJob = async (req, res) => {
   if (!job) {
     throw new NotFoundError(`No job with id ${jobId}`);
   }
-  res.status(StatusCodes.OK).send();
+  // res.status(StatusCodes.OK).send();
+  res.status(StatusCodes.OK).json({ msg: "The entry was deleted" });
 };
 
 module.exports = {
